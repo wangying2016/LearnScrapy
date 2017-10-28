2b2482584566a0b22593bf6e54224af0851e543e
Wang Ying <wy867504146@outlook.com>
2017-10-28 11:38:30 +0800

    Scrapy my blog optimized code

--- 4. blog/blog/spiders/blog-css.py
+++ 4. blog/blog/spiders/blog-css.py
@@ -3,20 +3,25 @@ import scrapy
 
 class BlogSpiderCss(scrapy.Spider):
     name = 'blog-css'
-    start_urls = [
-        'http://blog.csdn.net/u012814856'
-    ]
+
+    # 默认传入本人的博客名称，也可以使用 Spider 参数指定名称
+    # scrapy crawl blog-css -a user=xxx -o blog-css.xml
+    def __init__(self, user='u012814856', *args, **kwargs):
+        super(BlogSpiderCss, self).__init__(*args, **kwargs)
+        self.start_urls = [
+            'http://blog.csdn.net/%s' % user
+        ]
 
     def parse(self, response):
         for article in response.css('div.article_item'):
-            if len(article.css('font[color=red]')) > 0:
-                title = ''.join(article.css('span[class=link_title] a::text').extract())
-            else:
-                title = article.css('span.link_title a::text').extract_first()
             yield {
-               'title': title.replace('\r\n','').strip(),
-               'view': article.css('span.link_view::text').extract_first().replace('(', '').replace(')', ''),
-               'comment': article.css('span.link_comments::text').extract_first().replace('(', '').replace(')', ''),
+                # 这里要区分该标题是否置顶，如果是置顶的，解析出来就会有两个元素
+                # ['\r\n        ', '\r\n        思考的救赎（二）：三消游戏功能完善            \r\n        ']
+                # 此时使用 ''.join 即可转换为 str 然后去除空白元素即可
+                'title': ''.join(article.css('span.link_title a:not(font)::text').extract()
+                                ).replace('\r\n', '').strip(),
+                'view': article.css('span.link_view::text').extract_first().replace('(', '').replace(')', ''),
+                'comment': article.css('span.link_comments::text').extract_first().replace('(', '').replace(')', ''),
             }
             next_page = response.css('#papelist > a:nth-last-child(2)::attr(href)').extract_first()
             if next_page is not None:
--- 4. blog/blog/spiders/blog-xpath.py
+++ 4. blog/blog/spiders/blog-xpath.py
@@ -3,13 +3,21 @@ import scrapy
 
 class BlogSpiderXPath(scrapy.Spider):
     name = 'blog-xpath'
-    start_urls = [
-        'http://blog.csdn.net/u012814856'
-    ]
+
+    # 默认传入本人的博客名称，也可以使用 Spider 参数指定名称
+    # scrapy crawl blog-xpath -a user=xxx -o blog-xpath.xml
+    def __init__(self, user='u012814856', *args, **kwargs):
+        super(BlogSpiderXPath, self).__init__(*args, **kwargs)
+        self.start_urls = [
+            'http://blog.csdn.net/%s' % user
+        ]
 
     def parse(self, response):
         for article in response.xpath('//div[@class="list_item article_item"]'):
             yield {
+                # 这里要区分该标题是否置顶，如果是置顶的，解析出来就会有两个元素
+                # ['\r\n        ', '\r\n        思考的救赎（二）：三消游戏功能完善            \r\n        ']
+                # 此时使用 ''.join 即可转换为 str 然后去除空白元素即可
                 'title': ''.join(article.xpath('.//span[@class="link_title"]/a/text()'
                                                ).extract()).replace('\r\n', '').strip(),
                 'view': article.xpath('.//span[@class="link_view"]/text()'
